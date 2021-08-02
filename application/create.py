fedc5df418b3aa2c1bc6d79d7eec64c9b7de4965
한준규 <41336726+doongu@users.noreply.github.com>
2021-08-03 02:34:13 +0900

    check

--- application/create.py
+++ application/create.py
@@ -1,9 +1,12 @@
 # application/create.py
-# 서버 객체를 생성, 환경 변수를 세팅 해준다.
+# create.py 서버 객체를 생성하는 py파일
+
 from flask import Flask
 
-def create_app(mode='dev'):
+def create_app(mode='dev'): # 개발 모드인걸 단순히 명시 해놓은듯.
     app = Flask(__name__)
     if mode == 'dev':
-        app.config['DEBUG'] = True
-    return app
+        app.config['DEBUG'] = True # 디버그 모드를 활성화/비활성화 함  # 참고, https://flask-docs-kr.readthedocs.io/ko/latest/config.html
+        # 이런식으로도 되고 한번에 여러개를 하려면
+        # app.config.update( DEBUG = True, SECRET_KEY = '...' )
+    return app # Flask 객체를 return
--- application/run.py
+++ application/run.py
@@ -1,9 +1,13 @@
 #  application/run.py
 # 만들어진 서버 객체를 실행한다.
 
-from create import create_app
+from create import create_app # create.py에 create_app 함수를 import
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run() #cerate_app에서 DEBUG = True해놔서 여기서 별도로 설정해주지 않아도 된다.
+
+
+# 서버를 run 한 다음 켜졌는지 확인하는 방법은
+# 1. 직접접속, 2. curl로 날리기, 3. postman 등 요청 (그냥 http요청 해보는 것들 ㅇㅇ)
